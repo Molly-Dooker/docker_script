243028d7ecf9b44b488d28a3d8c262700b0ba9aa
lsm0729 <lsm0729@bos-semi.com>
2025-05-08 13:35:05 +0900

    ..

--- build.sh
+++ build.sh
@@ -11,17 +11,16 @@ docker rm -f ${docker_container_name} 2>/dev/null
 
 # Docker run command  
 docker run -it \
-  --name ${docker_container_name} \
-  --user $(id -u):$(id -g) \
-  -v /etc/passwd:/etc/passwd:ro \
-  -v /etc/group:/etc/group:ro \
-  -v ${HOME}:${HOME} \
-  -w ${HOME} \
-  -v "/local/mnt/workspace":"/local/mnt/workspace" \
-  -v ${DATADIR}:"/Data/Dataset/" \
-  -v /etc/localtime:/etc/localtime:ro \
-  --gpus all \
-  --shm-size=128g \
-  --ipc=host \
-  --hostname ${docker_container_name} \
-  ${docker_image_name}
+    --name ${docker_container_name} \
+    --user $(id -u):$(id -g) \
+    -v /etc/passwd:/etc/passwd:ro \
+    -v /etc/group:/etc/group:ro \
+    -v ${HOME}:${HOME} \
+    -w ${HOME} \
+    -v ${DATADIR}:"/Data/Dataset/" \
+    -v /etc/localtime:/etc/localtime:ro \
+    --gpus all \
+    --shm-size=128g \
+    --ipc=host \
+    --hostname ${docker_container_name} \
+    ${docker_image_name}
