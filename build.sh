88c2495fd981c9bf3acfcf1f20ffefff31e4427e
lsm0729 <lsm0729@bos-semi.com>
2025-05-08 09:06:49 +0900

    ..

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Define environment variables
-docker_image_name="cuda_12.1:v1.0"
-docker_container_name="cuda121"
-DATADIR=/data/dataset/
-# Ensure that a docker container named $docker_container_name does not already exist and remove it if it does
+docker_image_name="sungmin:v1.0"
+docker_container_name="sungmin_1.0"
+DATADIR=/Data/Dataset
+
 docker stop ${docker_container_name} 2>/dev/null && docker rm ${docker_container_name} 2>/dev/null
 
 
@@ -12,7 +12,14 @@ docker stop ${docker_container_name} 2>/dev/null && docker rm ${docker_container
 # Docker run command  
 docker run -it \
   --name ${docker_container_name} \
-  -v ${DATADIR}:"/data/dataset/" \
+  --user $(id -u):$(id -g) \
+  -v /etc/passwd:/etc/passwd:ro \
+  -v /etc/group:/etc/group:ro \
+  -v ${HOME}:${HOME} \
+  -w ${HOME} \
+  -v "/local/mnt/workspace":"/local/mnt/workspace" \
+  -v ${DATADIR}:"/Data/Dataset/" \
+  -v /etc/localtime:/etc/localtime:ro \
   --gpus all \
   --shm-size=128g \
   --ipc=host \
